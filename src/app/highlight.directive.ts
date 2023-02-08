2701733e4d9c7a3685bd1d51437fe54d645d4c35
Scott Phounpadith <sphounpadith@h4tg51svq6x3.acad.stclaircollege.scc>
2023-02-08 16:42:06 -0500

    add click listener to our directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,6 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, HostListener } from '@angular/core';
+
+
 @Directive({
   selector: '[appHighlight]' })
 
@@ -7,7 +9,17 @@ export class HighlightDirective implements OnInit {
   constructor(private elm: ElementRef) {
 
   }
+
+
   ngOnInit(): void{ //this way we know our colour isn't null
-    this.elm.nativeElement.style.backgroundColor = this.colour || "green";
+  }
+
+  @HostListener('click') onClick(btn: any){
+    this.highlightMe(this.colour);
+  }
+
+  private highlightMe(highlightColour: any){
+    this.elm.nativeElement.style.backgroundColor =
+    highlightColour || 'green';
   }
 }
